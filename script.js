ff8a0d2bd3a1c5843a94aaeab3c174ae6f15b9c5
Avatar <avatar@Avatars-MacBook-Pro.local>
2017-01-14 17:51:20 -0600

    Initial Commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function add(Number) {
+	Number += 2;
+	return Number;
+}
+
+function multiply(Number) {
+	Number *= 3;
+	return Number;
+}
+
+function subtract(Number) {
+	Number -= 3;
+	return Number;
+}
+
+var newNumber = add(muliply(subtract(6)));
